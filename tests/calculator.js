7074b4c64559da3cc2609db560aae6838c94d571
shwetas-lambdatest <shwetas@lambdatest.com>
2019-12-29 22:42:16 +0530

    my first commit

---
+++ tests/calculator.js
@@ -0,0 +1,25 @@
+describe('calculator test',function()                                                      //jasmine  
+{
+   it('addition test',function()                                                            //jasmine
+   {
+    browser.get('http://juliemr.github.io/protractor-demo/');                               //protractor
+   expect(browser.getCurrentUrl()).toBe('http://juliemr.github.io/protractor-demo/');
+
+    
+
+    element(by.model('first')).sendKeys('5');                //protractor
+
+    element(by.model('second')).sendKeys('8');              //protractor
+
+    element(by.css('[ng-click="doAddition()"]')).click();   //protractor
+
+    let input=element(by.cssContainingText('.ng-binding','13'));     //protractor
+    
+    expect(input.getText()).toEqual('13');    //jasmine
+
+    browser.sleep(2000);
+    
+
+   });
+
+});
