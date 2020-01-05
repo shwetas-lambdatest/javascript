d6125294cf2fc9bb15a06d1b60947249b6f5d533
shwetas-lambdatest <shwetas@lambdatest.com>
2020-01-05 08:01:41 +0530

    calculator homepage bypom commit

---
+++ pages/homepage.js
@@ -0,0 +1,48 @@
+let homepage=function()
+{   
+
+    
+    let first_no=element(by.model('first'));
+    let sec_no= element(by.model('second'));
+    let go_button=element(by.css('[ng-click="doAddition()"]'));
+
+
+
+
+    this.get=function(url){
+
+        browser.get(url);
+    }
+     
+    this.enter_firstno=function(FirstNo){
+
+        first_no.sendKeys(FirstNo);
+    };
+
+    this.enter_secondno=function(SecondNo){
+
+        sec_no.sendKeys(SecondNo);
+    };
+
+    this.enter_gobutton=function(){
+
+        go_button.click();
+
+    };
+
+
+     this.verify=function(result)
+     {
+        let output=element(by.cssContainingText('.ng-binding',result));     //protractor
+    
+        expect(output.getText()).toEqual(result);
+     };
+
+     browser.sleep(2000);
+
+
+
+   
+};
+
+module.export=new homepage();
